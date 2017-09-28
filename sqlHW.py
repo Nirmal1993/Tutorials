f284fed63b347b2545af3c68dd1c2d23e444d610
Nirmal <nirmalmanavalan18@gmail.com>
2017-09-27 22:58:32 -0400

    files were copied and replaced

--- sqlHW.py
+++ sqlHW.py
@@ -1,3 +1,14 @@
+import smtplib
+import time
+from email.utils import COMMASPACE, formatdate
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from getpass import getpass
+from smtplib import SMTP_SSL
+from email import encoders
+import mimetypes
+from email.mime.multipart import MIMEMultipart
 import pymysql
 from xlsxwriter.workbook import Workbook
 from tabulate import tabulate 
@@ -34,16 +45,46 @@ def excel_file():
     for r, rows in enumerate(cur.fetchall()):
         for c, cols in enumerate(rows):
             sheet.write(r, c, cols)
-def record_check():
-    Id=int(input("Enter ID"))
-    query=("select * from studentdetails where idno=%d"%(Id))
-    cur.execute(query)
-    data=cur.fetchall()
-    if not data:
-        print('Record doesnt exist')
-    else:
-        print('Record found')
-        print(data)
+def email_file():
+    fromaddr = input("Enter the from email address:")
+    toaddr = input("Enter the TO email address:")
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+    # storing the senders email address  
+    msg['From'] = fromaddr
+    # storing the receivers email address 
+    msg['To'] = toaddr
+    # storing the subject 
+    msg['Subject'] = "Subject of the Mail"
+    # string to store the body of the mail
+    body = "Body_of_the_mail"
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+    # open the file to be sent 
+    filename = "outfile.xlsx"
+    attachment = open("C:\\Users\\Nirmal Manavalan\\AppData\\Local\\Programs\\Python\\Python36-32\\outfile.xlsx", "rb")
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+    # encode into base64
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    # start TLS for security
+    s.starttls()
+    # Authentication
+    s.login(fromaddr, "windows2016")
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+    # terminating the session
+    s.quit()
+      
 while True:
    print("Enter option:")
    print("1. Add")
@@ -51,7 +92,7 @@ while True:
    print("3. Search")
    print("4. Delete")
    print("5. Export to excel")
-   print("6.Check if record exists by entering ID")
+   print("6.Email the file")
    print("7. Quit")
    opt=int(input("Enter your option::"))
    if opt==1:
@@ -67,7 +108,7 @@ while True:
    elif opt==5:
         excel_file()
    elif opt==6:
-        record_check()
+        email_file()
    elif opt==7:
         break;
    else:
